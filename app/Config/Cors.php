ab0847ab2a013618a7ce702e2e191e6c3cc099b9
ofarook060 <ofarook060@gmail.com>
2026-07-16 05:55:00 +0000

    121

--- app/Config/Cors.php
+++ app/Config/Cors.php
@@ -34,7 +34,7 @@ class Cors extends BaseConfig
          *   - ['http://localhost:8080']
          *   - ['https://www.example.com']
          */
-        'allowedOrigins' => ['*'],
+        'allowedOrigins' => ['https://sykmm.site'],
 
         /**
          * Origin regex patterns for the `Access-Control-Allow-Origin` header.
